3c05d7b81ac60dce7e7177f01c4f24a5f1dbc91b
Pablo J. Torres Hernández <pablojth@gmail.com>
2022-01-20 13:34:52 +0100

    Modificaciones iniciales y primera parte del ejercico

--- practicaGeneral.R
+++ practicaGeneral.R
@@ -1,5 +1,14 @@
 #Script de la práctica
 
+#Instalacion y carga de paquetes
 #install.packages("palmerpenguins")
 library(palmerpenguins)
-data(package = "palmerpenguins")
+library(dplyr)
+
+#Carga de los datos
+data(package = "palmerpenguins", penguins)
+
+#Número de individuos totales, masculinos y femeninos por especie
+penguins_sex <- penguins %>% 
+  select(species, sex) %>% 
+  count(sex)
