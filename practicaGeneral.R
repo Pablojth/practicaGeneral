96cc9f1c234b2ae1dc883389469af2c404df53cf
Pablo J. Torres Hernández <pablojth@gmail.com>
2022-01-21 13:13:02 +0100

    Responder a las preguntas del ejercicio 2

--- practicaGeneral.R
+++ practicaGeneral.R
@@ -4,17 +4,18 @@
 #install.packages("palmerpenguins")
 library(palmerpenguins)
 library(dplyr)
+library(plyr)
+library(ggplot2)
 
 #Carga de los datos
 data(package = "palmerpenguins", penguins)
-
 
 ##Ejercicio 1##
 
 #Número de individuos totales, masculinos y femeninos por especie
 penguins_sex <- penguins %>% 
-  select(species, sex) %>% 
-  count(sex) %>% 
+  select(species, sex)%>% 
+  dplyr::count(species)%>% 
   glimpse()
 
 #La media, desviación estándar, valor mínimo y máximo de la longitud y profundidad del pico, 
@@ -37,7 +38,26 @@ penguins_stats <- penguins %>%
             mean_body_mass = mean(body_mass_g, na.rm = TRUE),
             sd_body_mass = sd(body_mass_g, na.rm = TRUE),
             min_body_mass = min(body_mass_g, na.rm = TRUE),
-            max_body_mass = max(body_mass_g, na.rm = TRUE),) %>%
+            max_body_mass = max(body_mass_g, na.rm = TRUE)) %>%
   glimpse()
 
 ##Ejercicio 2##
+
+#Un gráfico de barras que represente el número de individuos muestreados de cada especie en cada isla,
+#representando las especies en diferentes colores (chinstrap – morado, gentoo – azul, adelie – naranja)
+
+graf_stats <- penguins %>% 
+  select(species, island) 
+
+ggplot(data=graf_stats) +
+  geom_bar(aes(x=island, fill=species)) +
+  scale_fill_manual(values=c("darkorange", "mediumorchid", "mediumblue"))
+
+summary(graf_stats)
+#Contesta a las siguientes preguntas: ¿qué especie se ha muestreado en las tres islas? 
+
+print("La especie Adelie se ha muestreado en las tres islas")
+
+#¿cuántos individuos se han muestreado de la isla Dream?
+
+print("En la isla de dream se han muestreado 124 individuos")
